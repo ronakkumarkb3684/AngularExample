e16567338a30661ee0c4497ad56130992bdce10c
Ronak <ronak27brahmbhatt@gmail.com>
2020-03-22 14:08:27 -0400

    deleted about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
     ContactComponent,
     HomeComponent
   ],
